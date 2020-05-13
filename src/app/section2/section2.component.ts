b9b877efee1209846028d2b6fd019a96ebb65caf
produde <prodyut095@gmail.com>
2020-05-13 13:07:40 +0530

    menu

--- src/app/section2/section2.component.ts
+++ src/app/section2/section2.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-section2',
   templateUrl: './section2.component.html',
   styleUrls: ['./section2.component.scss']
 })
-export class Section2Component implements OnInit {
 
+export class Section2Component implements OnInit {
+  navFixed = false;
+  private scrollOffset = 808;
   constructor() { }
 
   ngOnInit() {
   }
-
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.navFixed = (window.pageYOffset
+      || document.documentElement.scrollTop
+      || document.body.scrollTop || 0
+    ) > this.scrollOffset;
+  }
 }
