8877f10b9a244d730f994cc29f90d511945e6fe5
Prodyut Das <prodyut.das@capgemini.com>
2020-07-19 02:06:45 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { Section1Component } from './section1/section1.component';
 import { Section2Component } from './section2/section2.component';
 import * as $ from 'jquery';
 import { SectionHomeComponent } from './section2/section-home/section-home.component';
+import { SectionContactComponent } from './section2/section-contact/section-contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Section1Component,
     Section2Component,
-    SectionHomeComponent
+    SectionHomeComponent,
+    SectionContactComponent
   ],
   imports: [
     BrowserModule,
