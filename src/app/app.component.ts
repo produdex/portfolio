06ae2d7d1d5490c84f690d1d81069d3e8478b75f
Prodyut Das <prodyut.das@capgemini.com>
2020-07-20 01:25:52 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,10 @@ export class AppComponent implements OnInit {
   num1: number;
   num2: number;
   result: number;
+  isShow: boolean;
+  topPosToStartShowing = 1;
   ngOnInit() {
+
     setTimeout(this.showPage, 3000);
     this.hireMe();
   }
@@ -35,4 +38,30 @@ export class AppComponent implements OnInit {
       overlay.classList.remove('show');
     });
   }
+  @HostListener('window:scroll')
+  checkScroll() {
+
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
 }
